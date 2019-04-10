2c03e6499e5fd48309994415454b23835a894255
Alec Budd <abudd1094@gmail.com>
2019-04-10 15:48:53 +0200

    route added with function that splits query to an array

--- app.js
+++ app.js
@@ -7,5 +7,19 @@ var port = process.env.PORT || 3000;
 const nonLexicalWords = ["to", "got", "is", "have", "and", "although", "or", "that", "when", "while", "a", "either", "more", "much", "neither", "my", "that", "the", "as", "no", "nor", "not", "at", "between", "in", "of", "without", "I", "you", "he", "she", "it", "we", "they", "anybody", "one"];
 
 
+// BASIC FUNCTION: Returns lexical density for entire input
+function lexicalFilter(input) { 
+  var inputWordsArray = input.split(' ');
+
+  return inputWordsArray;
+}
+
+
+// ROUTE
+app.get('/complexity', function(req, res) {
+  var input = req.query.input;
+  
+  res.send(lexicalFilter(input));
+});
 
 app.listen(port);
